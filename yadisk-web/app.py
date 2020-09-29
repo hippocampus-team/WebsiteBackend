9bf1b72d5e77def917f9be9a00895659e3efd53a
DablSi <duck.shelter@gmail.com>
2020-09-29 17:57:01 +0300

    Getting JSONs from yandex disk

---
+++ yadisk-web/app.py
@@ -0,0 +1,27 @@
+import os
+
+import yandexwebdav
+from aiohttp import web
+
+FOLDER = u'/resume/'
+conf = yandexwebdav.Config({
+    "user": os.environ.get('YANDEX_LOGIN'),
+    "password": os.environ.get('YANDEX_PASSWORD')
+})
+
+
+async def get_json(request):
+    files = conf.list(FOLDER)
+    response = ''
+    jsons = list()
+    for file in files:
+        if len(file.keys()):
+            jsons.append(conf.download(list(file.keys())[0]).decode('utf-8'))
+    for j in jsons:
+        response += j + '\n'
+    return web.Response(text=response)
+
+
+app = web.Application()
+app.router.add_get('/json', get_json)
+web.run_app(app, host='localhost')
